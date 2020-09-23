82043f57c9583f9c631323c97c2ccfb265ec9409
eduardodelduqui <66086087+eduardodelduqui@users.noreply.github.com>
2020-09-23 01:17:21 -0300

    Iniciando projeto

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+body, html, #app{
+	height: 100%;
+	margin: 0;
+	padding: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="corpo-home">
+		<div class="box-lista">
+			<ul>
+				<li v-for="loja in lojas" class="loja">
+					<!-- {{loja.nome}}
+					{{loja.tipo}} -->
+				</li>
+			</ul>
+		
+
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+	data() {
+		return {
+			lojas: []
+			
+		}
+	},
+
+	
+
+	created(){
+		let promise = this.$http.get('http://localhost:3030/lojas')
+		promise.then((res) => res.json())
+			.then((lojas) => {
+				this.lojas = lojas
+			});
+			
+
+	},
+
+}
+</script>
+
+<style scoped>
+
+.corpo-home
+{
+	background-color: rgb(176, 58, 50);
+	height: 100%;
+}
+
+.box-lista{
+	position: absolute;
+	background-color: white;
+	width: 90%;
+	height: 100%;
+	transform: translate(-50%, -50%);
+	top: 50%;
+	left: 50%;
+	margin: 80px 0 0 0px;
+	padding: 0px;
+	border-radius: 20px;
+	padding: 30px;
+}
+
+ul{
+	list-style: none
+}
+
+
+
+</style>
